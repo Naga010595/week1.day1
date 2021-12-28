0c1e16bfa7ea86c1f110d09a54ab84a1194bfb88
Naga010595 <96667939+Naga010595@users.noreply.github.com>
2021-12-28 22:32:48 +0300

    Add files via upload

---
+++ LearnIfCondition.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+
+public class LearnIfCondition {
+
+	public static void main(String[] args) {
+		int x = 95;
+	if(x>=35) {
+		System.out.println("PASS");
+	}
+	else if(x>=90){
+		System.out.println("DISCTINCTION");
+	}
+	else {
+		System.out.println("FAIL");
+	}
+	}
+
+}
